0474f5e6d7f03076f41343668997539f99438b66
Hans Henrik Grønsleth <henrik.gronsleth@knowit.no>
2019-03-01 16:31:27 +0100

    Update splash text

--- src/pages/SplashPage/SplashPage.jsx
+++ src/pages/SplashPage/SplashPage.jsx
@@ -19,9 +19,9 @@ const SplashPage = () => (
       <p className={styles.SubHeader}>27. {HTMLEntities.ndash} 29. juni 2019</p>
       <Bounce>
         <p>
-          <em>Psst!</em> Vi har noen få Early Bird-pass til rabattert pris. De
-          er egentlig ikke lansert ennå, men siden du er her kan vi jo røpe det
-          for deg.
+          Hei der! Frem til <strong>15. mars</strong> kan du kjøpe rabatterte
+          festivalpass til Mandaljazz 2019! «Early Bird», som kidsa sier. Vær
+          tidlig ute!
         </p>
         <FontAwesomeIcon icon={faSmileWink} color="mediumseagreen" size="2x" />
       </Bounce>
