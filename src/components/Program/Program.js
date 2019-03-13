570456f06e805ae51e1e0ada0c565ae7c1c95790
hanshenrik <hh.gronsleth@gmail.com>
2019-03-14 00:49:17 +0100

    Improve program, make it show/hide-able

--- src/components/Program/Program.js
+++ src/components/Program/Program.js
@@ -3,54 +3,71 @@ import capitalize from "lodash/capitalize";
 import groupBy from "lodash/groupBy";
 import sortBy from "lodash/sortBy";
 import dayjs from "dayjs";
+import { SlideDown } from "react-slidedown";
+import "react-slidedown/lib/slidedown.css";
 
 import { HashLink } from "../";
 import styles from "./Program.css";
 import artists from "../Artists/data.json";
 
-const Program = () => {
-  return (
-    <div className={styles.ProgramWrapper}>
-      <div className={styles.Program}>
-        {Object.entries(
-          groupBy(sortBy(artists, "concertStartAt"), artist =>
-            capitalize(dayjs(artist.concertStartAt).format("dddd"))
-          )
-        ).map(([day, artists]) => (
-          <div key={day}>
-            <h2 className="with-background">{day}</h2>
-            <ul className={styles.ProgramList}>
-              {sortBy(artists, "concertStartAt").map(artist => (
-                <li key={artist.id}>
-                  <span className={styles.ConcertInfo}>
-                    {dayjs(artist.concertStartAt).format("HH:mm")} @{" "}
-                    {artist.venue}
-                  </span>
-                  {" / "}
-                  <span className={styles.ArtistName}>
-                    {artist.hideFromArtistList ? (
-                      <strong>{artist.shortName || artist.name}</strong>
-                    ) : (
-                      <HashLink
-                        smooth
-                        to={{
-                          pathname: "/",
-                          hash: `#${artist.id}`,
-                          state: { activeId: artist.id }
-                        }}
-                      >
-                        <strong>{artist.shortName || artist.name}</strong>
-                      </HashLink>
-                    )}
-                  </span>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </div>
+class Program extends React.Component {
+  state = {
+    isOpen: false
+  };
+
+  toggleIsOpen = () => this.setState({ isOpen: !this.state.isOpen });
+
+  renderToggleIsOpenButton = () => (
+    <div onClick={this.toggleIsOpen} className={styles.OpenProgramButton}>
+      {this.state.isOpen ? "Lukk" : "Se"} program
     </div>
   );
-};
+
+  render() {
+    return (
+      <div className={styles.ProgramWrapper}>
+        <SlideDown className={styles.Program} closed={!this.state.isOpen}>
+          {Object.entries(
+            groupBy(sortBy(artists, "concertStartAt"), artist =>
+              capitalize(dayjs(artist.concertStartAt).format("dddd"))
+            )
+          ).map(([day, artists]) => (
+            <div key={day}>
+              <h4 className="with-background">{day}</h4>
+              <ul className={styles.ProgramList}>
+                {sortBy(artists, "concertStartAt").map(artist => (
+                  <li key={artist.id}>
+                    <span className={styles.ConcertInfo}>
+                      {dayjs(artist.concertStartAt).format("HH:mm")} @{" "}
+                      {artist.venue}
+                    </span>
+                    {" / "}
+                    <span className={styles.ArtistName}>
+                      {artist.hideFromArtistList ? (
+                        <strong>{artist.shortName || artist.name}</strong>
+                      ) : (
+                        <HashLink
+                          smooth
+                          to={{
+                            pathname: "/",
+                            hash: `#${artist.id}`,
+                            state: { activeId: artist.id }
+                          }}
+                        >
+                          <strong>{artist.shortName || artist.name}</strong>
+                        </HashLink>
+                      )}
+                    </span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </SlideDown>
+        {this.renderToggleIsOpenButton()}
+      </div>
+    );
+  }
+}
 
 export default Program;
