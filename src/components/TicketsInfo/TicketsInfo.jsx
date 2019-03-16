ab6db97c90e447681bfd4600646ffb658bc13625
hanshenrik <hh.gronsleth@gmail.com>
2019-03-16 23:36:14 +0100

    Use larger heading for ticket info

--- src/components/TicketsInfo/TicketsInfo.jsx
+++ src/components/TicketsInfo/TicketsInfo.jsx
@@ -10,7 +10,7 @@ const TicketsInfo = () => (
       Mandaljazz tilbyr enkeltbilletter, dagspass og festivalpass til publikum.
     </p>
 
-    <h4>Hvordan får jeg tak i billetter?</h4>
+    <h3>Hvordan får jeg tak i billetter?</h3>
     <p>
       Billetter kjøpes på{" "}
       <Link href="https://mandaljazz.ticketco.events/no/nb/e/mandaljazz-2019">
@@ -20,26 +20,26 @@ const TicketsInfo = () => (
       standen i gågata.
     </p>
 
-    <h4>Festivalpass og dagspass</h4>
+    <h3>Festivalpass og dagspass</h3>
     <p>Gir inngang til alle konserter.</p>
 
-    <h4>Studenter og ungdom under 26 år</h4>
+    <h3>Studenter og ungdom under 26 år</h3>
     <p>
       Mandaljazz tilbyr rabatterte billetter til studenter og ungdom under 26
       år. Studenter inkluderer også elever på videregående, ungdoms- og
       barneskoler.
     </p>
 
-    <h4>Barn</h4>
+    <h3>Barn</h3>
     <p>Alle barn under 12 år går gratis på Mandaljazz.</p>
 
-    <h4>Funksjonshemmede med ledsagerbevis</h4>
+    <h3>Funksjonshemmede med ledsagerbevis</h3>
     <p>
       Innehavere av ledsagerbevis for funksjonshemmede gis gratis billett til
       sin ledsager.
     </p>
 
-    <h4>Aldersgrenser</h4>
+    <h3>Aldersgrenser</h3>
     <p>
       Alle konserter i <strong>Teltet</strong> har 18 års aldersgrense. Barn
       under 18 år har adgang i følge med voksen.
