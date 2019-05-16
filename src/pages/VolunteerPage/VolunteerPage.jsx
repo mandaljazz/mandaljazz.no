a703c2325b402aaf7025cddbcd90800d9e3f2d47
hanshenrik <hh.gronsleth@gmail.com>
2019-05-16 22:13:39 +0200

    Fix warnings

--- src/pages/VolunteerPage/VolunteerPage.jsx
+++ src/pages/VolunteerPage/VolunteerPage.jsx
@@ -50,9 +50,9 @@ const VolunteerPage = () => (
       src="https://docs.google.com/forms/d/e/1FAIpQLScCyYzI5qiN63YSyjv2OJxi6cAvVxRDXSU6k4oDWd-3x8gNdw/viewform?embedded=true"
       width="100%"
       height="1934"
-      frameborder="0"
-      marginheight="0"
-      marginwidth="0"
+      frameBorder="0"
+      marginHeight="0"
+      marginWidth="0"
     >
       Henter skjema...
     </iframe>
