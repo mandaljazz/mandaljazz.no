5351941cd84f6bd049dd256901df4b8d0680f16f
hanshenrik <hh.gronsleth@gmail.com>
2019-06-11 22:07:44 +0200

    More spacing on ProgramPage

--- src/pages/ProgramPage.jsx
+++ src/pages/ProgramPage.jsx
@@ -3,10 +3,10 @@ import React from "react";
 import { Program, ScrollToTopOnMount } from "../components";
 
 const ProgramPage = () => (
-  <>
+  <div style={{ marginTop: "2rem" }}>
     <ScrollToTopOnMount />
     <Program isAlwaysOpen />
-  </>
+  </div>
 );
 
 export default ProgramPage;
