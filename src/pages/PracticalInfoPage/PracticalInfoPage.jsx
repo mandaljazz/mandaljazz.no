0b9702a4a567ae531f19677f1b72b1d3977d30d5
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2020-05-25 14:46:01 +0200

    Move 2019 map to bottom

--- src/pages/PracticalInfoPage/PracticalInfoPage.jsx
+++ src/pages/PracticalInfoPage/PracticalInfoPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import {
   TravelInfo,
   AccomodationInfo,
-  ScrollToTopOnMount
+  ScrollToTopOnMount,
 } from "../../components";
 import styles from "./PracticalInfoPage.css";
 import mapImg from "../../assets/images/kart.jpg";
@@ -11,10 +11,10 @@ import mapImg from "../../assets/images/kart.jpg";
 const PracticalInfoPage = () => (
   <div className={styles.PracticalInfoPage}>
     <ScrollToTopOnMount />
-    <h1>Festivalkart 2019</h1>
-    <img src={mapImg} alt="Kart" style={{ width: "100%", marginTop: "1rem" }} />
     <TravelInfo />
     <AccomodationInfo />
+    <h1>Festivalkart 2019</h1>
+    <img src={mapImg} alt="Kart" style={{ width: "100%", marginTop: "1rem" }} />
   </div>
 );
 
