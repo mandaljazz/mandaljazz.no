46fd2cd113a368d340527bd54f7631dd77586f7a
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2020-05-25 23:56:27 +0200

    Close nav menu on click on mobile

--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -51,13 +51,13 @@ class NavMenu extends React.Component {
               <span onClick={this.close} className={styles.BurgerMenuCloseIcon}>
                 {HTMLEntities.multiplicationX}
               </span>
-              <RouterLink to="/">Artister</RouterLink>
-              <RouterLink to="/billetter">Billetter</RouterLink>
-              <RouterLink to="/program">Program</RouterLink>
-              <RouterLink to="/praktisk">Praktisk</RouterLink>
-              <RouterLink to="/frivillig">Frivillig</RouterLink>
-              <RouterLink to="/historikk">Historikk</RouterLink>
-              <RouterLink to="/koronainfo">Koronainfo</RouterLink>
+              <RouterLink onClick={this.close} to="/">Artister</RouterLink>
+              <RouterLink onClick={this.close} to="/billetter">Billetter</RouterLink>
+              <RouterLink onClick={this.close} to="/program">Program</RouterLink>
+              <RouterLink onClick={this.close} to="/praktisk">Praktisk</RouterLink>
+              <RouterLink onClick={this.close} to="/frivillig">Frivillig</RouterLink>
+              <RouterLink onClick={this.close} to="/historikk">Historikk</RouterLink>
+              <RouterLink onClick={this.close} to="/koronainfo">Koronainfo</RouterLink>
             </div>
           </OffCanvas>
         </>
