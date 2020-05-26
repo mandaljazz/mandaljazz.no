88f8c374038708402fd78ee45631d4f46c35f490
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2020-05-27 01:20:59 +0200

    Fix background on mobile menu

--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -39,7 +39,7 @@ class NavMenu extends React.Component {
             width="250px"
             style={{
               content: {
-                background: "var(--superlighteal)",
+                background: "var(--superlightpeach)",
                 boxShadow: "-1px 0 4px 0 var(--gray)",
               },
             }}
