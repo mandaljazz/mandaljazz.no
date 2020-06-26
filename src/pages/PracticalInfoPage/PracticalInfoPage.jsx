9d0531aa4c7115ede40261f5148f83f04bd0701a
hanshenrik <hh.gronsleth@gmail.com>
2020-06-26 13:18:41 +0200

    Add map

--- src/pages/PracticalInfoPage/PracticalInfoPage.jsx
+++ src/pages/PracticalInfoPage/PracticalInfoPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 
 import {
   TravelInfo,
@@ -8,13 +9,39 @@ import {
 import styles from "./PracticalInfoPage.css";
 import mapImg from "../../assets/images/kart.jpg";
 
+const Map = ReactMapboxGl({
+  accessToken:
+    "pk.eyJ1IjoiaGFuc2hlbnJpayIsImEiOiJjaXpjdTc5a3AwMDhrMndwZW1ucWdkdXdwIn0.a15J4-cKGPIZClcvh-LeTQ",
+});
+
 const PracticalInfoPage = () => (
   <div className={styles.PracticalInfoPage}>
     <ScrollToTopOnMount />
+    <Map
+      style="mapbox://styles/mapbox/light-v9"
+      center={[7.457844, 58.025007]}
+      zoom={[13]}
+      containerStyle={{
+        height: "50vh",
+        width: "100%",
+      }}
+    >
+      <Layer
+        type="circle"
+        id="marker"
+        paint={{
+          "circle-radius": 13,
+          "circle-color": "#f89876",
+          "circle-stroke-color": "#f1755b",
+          "circle-stroke-width": 2,
+          "circle-opacity": 0.9,
+        }}
+      >
+        <Feature coordinates={[7.457844, 58.025007]} />
+      </Layer>
+    </Map>
     <TravelInfo />
     <AccomodationInfo />
-    <h1>Festivalkart 2019</h1>
-    <img src={mapImg} alt="Kart" style={{ width: "100%", marginTop: "1rem" }} />
   </div>
 );
 
