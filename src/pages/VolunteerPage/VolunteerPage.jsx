348f3d995837a20bb097498b5555bc621b301e7f
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2021-03-28 19:29:14 +0200

    Add captions to volunteer images

--- src/pages/VolunteerPage/VolunteerPage.jsx
+++ src/pages/VolunteerPage/VolunteerPage.jsx
@@ -37,8 +37,7 @@ const VolunteerPage = () => (
       Beklager, her skulle det egentlig vært en video, men nettleseren din
       støtter ikke innebygde videoer :(
     </video>
-    <ScrollToFormButton />
-    <div style={{ padding: "1rem", maxWidth: "720px" }}>
+    <div style={{ padding: "1rem", width: "100%", maxWidth: "720px" }}>
       <p>Hvilken frivilliggruppe vil du være med i?</p>
       <Collapsible title="Artistvertskap">
         <div>
@@ -105,6 +104,9 @@ const VolunteerPage = () => (
           smittevern blir etterfulgt.
         </div>
       </Collapsible>
+    </div>
+    <ScrollToFormButton />
+    <div style={{ padding: "1rem", width: "100%", maxWidth: "720px" }}>
       <Blockquote person="Håvard (bar og servering)">
         Hadde det ikke vært for at man måtte okkupere den beste plassen i
         teltet, høre på det siste skriket innen jazz, omgås sprudlende fornøyde
@@ -114,13 +116,19 @@ const VolunteerPage = () => (
         hadde det faktisk vært helt greit å jobbe i baren; det er nemlig ganske
         chill å tappe øl!
       </Blockquote>
+      <figure>
       <img src={volunteers2019Img} alt="Frivilliggjengen 2019" />
+        <figcaption>Frivilliggjengen 2019</figcaption>
+      </figure>
       <Blockquote person="Marte (sjau og festivalområde)">
         Sjauen er en blanding av løfting og bæring og svette og snekring og en
         heil haug med dårlige vitser. På en bra måte! Derfor er sjau bare
         dritgøy.
       </Blockquote>
-      <img src={volunteers2018Img} alt="Frivilliggjengen 2018" />
+      <figure>
+        <img src={volunteers2018Img} alt="Frivilliggjengen 2018" />
+        <figcaption>Frivilliggjengen 2018</figcaption>
+      </figure>
       <Blockquote person="Helga (billett og merch)">
         Som frivillig i billettcrewet blir du med i en gjeng med mye moro og
         godt samhold. Du blir kjent med masse kjekke folk og du kjenner at du
@@ -129,39 +137,41 @@ const VolunteerPage = () => (
         <br />
         Vi har mange forskjellige og gøyale oppgaver og ingen vakt er lik!
       </Blockquote>
+      <figure style={{ marginBottom: "3rem" }}>
+        <img src={volunteers2016Img} alt="Frivilliggjengen 2016" />
+        <figcaption>Frivilliggjengen 2016</figcaption>
+      </figure>
+      <div id="frivilligskjema" />
+      <iframe
+        id="volunteer-form"
+        title="Frivilligskjema"
+        src="https://docs.google.com/forms/d/e/1FAIpQLSfckhDBZ8adOzk_XoDSeLtvj37KGexnRMI6lWKJR1xA19HOug/viewform?embedded=true"
+        width="100%"
+        height="2200"
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
+      >
+        Henter skjema…
+      </iframe>
+      <figure>
+        <img src={volunteers2014Img} alt="Frivilliggjengen 2014" />
+        <figcaption>Frivilliggjengen 2014</figcaption>
+      </figure>
+      <Blockquote person="Mari Marie (transport)">
+        For en mulighet til å bli kjent med masse forskjellige folk på kjøretur!
+        <br />
+        <br />
+        Det å få tid i en bil med interessante, rare, sære, kule, kunstneriske,
+        motiverende og hyggelige musikere er jo ikke noe man får hver dag. Og på
+        veien for å hente artister kan du få litt pustetid og blæste noe musikk!
+        <br />
+        <br />
+        Kjøretidene er ofte veldig greie som gjør at du er ledig under mange av
+        konsertene. Og veldig mye mer chill fysisk hvis du ikke er så fan av å
+        bære tungt eller stå lenge.
+      </Blockquote>
     </div>
-    <img
-      src={volunteers2016Img}
-      alt="Frivilliggjengen 2016"
-      style={{ marginBottom: "3rem" }}
-    />
-    <div id="frivilligskjema" />
-    <iframe
-      id="volunteer-form"
-      title="Frivilligskjema"
-      src="https://docs.google.com/forms/d/e/1FAIpQLSfckhDBZ8adOzk_XoDSeLtvj37KGexnRMI6lWKJR1xA19HOug/viewform?embedded=true"
-      width="100%"
-      height="2200"
-      frameBorder="0"
-      marginHeight="0"
-      marginWidth="0"
-    >
-      Henter skjema…
-    </iframe>
-    <img src={volunteers2014Img} alt="Frivilliggjengen 2014" />
-    <Blockquote person="Mari Marie (transport)">
-      For en mulighet til å bli kjent med masse forskjellige folk på kjøretur!
-      <br />
-      <br />
-      Det å få tid i en bil med interessante, rare, sære, kule, kunstneriske,
-      motiverende og hyggelige musikere er jo ikke noe man får hver dag. Og på
-      veien for å hente artister kan du få litt pustetid og blæste noe musikk!
-      <br />
-      <br />
-      Kjøretidene er ofte veldig greie som gjør at du er ledig under mange av
-      konsertene. Og veldig mye mer chill fysisk hvis du ikke er så fan av å
-      bære tungt eller stå lenge.
-    </Blockquote>
     <p>Vi gleder oss til å møte deg :)</p>
   </div>
 );
