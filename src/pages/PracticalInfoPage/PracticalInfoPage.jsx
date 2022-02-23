5a287230bb6f761326cd1acadf976ef4332c2ebd
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2022-02-23 09:35:13 +0100

    Limit width for text paragraphs

--- src/pages/PracticalInfoPage/PracticalInfoPage.jsx
+++ src/pages/PracticalInfoPage/PracticalInfoPage.jsx
@@ -46,25 +46,27 @@ const PracticalInfoPage = () => (
         <em>Keiser Nicolaus gate 8–12, 4515 Mandal</em>
       </Link>
     </p>
-    <TravelInfo />
-    <AccomodationInfo />
-    <h1>Universell utforming</h1>
-    <p>
-      De fleste av konsertene skjer på vårt festivalområdet eller i vårt
-      festivaltelt på Kulturfabrikken.
-    </p>
-    <p>
-      Kulturfabrikken har to handicap-toaletter i første etasje. Det er montert
-      rampe for å enkelt komme inn i bygget med rullestol. Utendørs i bakgården
-      er det grus, så det kan kanskje være litt mer krevende å komme til med
-      rullestol enn på asfalt eller andre underlag, men vi har alltid frivillige
-      på jobb som kan bistå ved behov slik at rullestolbrukere fint skal komme
-      seg inn i teltet eller rundt på området.
-    </p>
-    <p>
-      Vi arrangerer også noen konserter i kulturhuset Buen. Her er det heis og
-      handicap-toaletter.
-    </p>
+    <div className={styles.TextWidth}>
+      <TravelInfo />
+      <AccomodationInfo />
+      <h1>Universell utforming</h1>
+      <p>
+        De fleste av konsertene skjer på vårt festivalområdet eller i vårt
+        festivaltelt på Kulturfabrikken.
+      </p>
+      <p>
+        Kulturfabrikken har to handicap-toaletter i første etasje. Det er
+        montert rampe for å enkelt komme inn i bygget med rullestol. Utendørs i
+        bakgården er det grus, så det kan kanskje være litt mer krevende å komme
+        til med rullestol enn på asfalt eller andre underlag, men vi har alltid
+        frivillige på jobb som kan bistå ved behov slik at rullestolbrukere fint
+        skal komme seg inn i teltet eller rundt på området.
+      </p>
+      <p>
+        Vi arrangerer også noen konserter i kulturhuset Buen. Her er det heis og
+        handicap-toaletter.
+      </p>
+    </div>
   </div>
 );
 
